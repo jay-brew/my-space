e473820f962d07b4a4a145886d697f59a786bc7d
jay-brew <brew0@naver.com>
2022-10-13 21:39:36 +0900

    소스 보완

--- server/index.js
+++ server/index.js
@@ -5,8 +5,29 @@ const port = 4000; // 겹치지 않게
 const db = require('./config/db');
 const cors = require("cors");
 
-app.use(express.json())
+const {sign} = require('jsonwebtoken');
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+const saltRounds = 10;
+const plainTextPassword1 = 'asdf1234';
+const plainTextPassword2 = 'qwer1234';
+
+require('dotenv').config();
+
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// const {hashSync, getSaltSync, compareSync} = require('bcrypt');
+
+app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended : false
+}));
+app.use(cookieParser());
+app.use(express.static('public'));
+// app.engine("hbs",exphbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 app.get("/api/get", (req,res) => {
     const sql = "SELECT * FROM login";
@@ -16,7 +37,14 @@ app.get("/api/get", (req,res) => {
 });
 
 app.post("/signup", (req,res) => {
-    console.log("/signup", req.body);
+    const salt = genSaltSync(10);
+    req.body.id = hashSync(req.body.id, salt);
+
+    const jsontoken = sign({ id: req.body.id }, "SECRET_TOKEN" , {
+        expiresIn: "1h"
+      });
+      
+    console.log("jsontoken : ", jsontoken);
     var idx = req.body.idx;
     var id = req.body.id;
     var password = req.body.password;
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [id, setID] = useState(null); // 아이디
+  const [password, setPassword] = useState(null);    // 비밀번호
+  const nameInput = useRef();// nameInput 객체생성 useRef();호출
+
   const navigate = useNavigate();
 
+  // 로그인 버튼 클릭 시 수행
   const loginBtnClick = () => {
-    navigate("/Home");
+    if(id === null) {
+      console.log("???")
+      alert("아이디를 입력해 주세요.");
+      nameInput.current.focus();//focus api 사용 //curre
+      return false;
+    } else if(password === null) {
+      alert("비밀번호를 입력해 주세요.");
+      nameInput.current.focus();//focus api 사용 //curre
+      return false;
+    } else {
+      navigate("/Home");
+    }
   };
 
   const signupButton = () => {
-    navigate("/Signup")
+    navigate("/Signup");
   };
   
   return (
     <div>
       <div className='login'>
-        <div className='loginBox'>
-          <div>ID : <input type="text" /></div>
-          <div>PW : <input type="text" /></div>
-          <div><a style={{border:"2px solid orange", cursor:"pointer"}} onClick={()=>{signupButton()}}>회원가입</a><a>비밀번호찾기</a><button onClick={()=>{loginBtnClick()}}>Login</button></div>
+        <div className='loginBorderBox'>
+          <div><input type="text" id='id' placeholder='아이디를 입력해 주세요.' onChange={(event) => setID(event.target.value)} ref={nameInput}/></div>
+          <div><input type="text" id='password' placeholder='비밀번호를 입력해 주세요.' onChange={(event) => setID(event.target.value)} ref={nameInput}/></div>
+          <div><button onClick={()=>{loginBtnClick()}}>Login</button></div>
+          <div><a onClick={()=>{signupButton()}}>회원가입</a><a>비밀번호찾기</a></div>
         </div>
       </div>
     </div>
