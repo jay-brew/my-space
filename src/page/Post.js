7c1d4a89b9a15ece1535f9255f7dfdd9c378f9c4
jay-brew <brew0@naver.com>
2022-12-19 21:36:15 +0900

    study, post update

--- src/page/Post.js
+++ src/page/Post.js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import {useLocation, useNavigate } from 'react-router-dom';
 
-const Post = () => {
-    const location = useLocation();
+const Post = (props) => {
+    // const location = useLocation();
     const [post, setPost] = useState("");
     const [inputDisable, setInputDisable] = useState(true);
     const [title, setTitle] = useState("");
@@ -13,14 +13,23 @@ const Post = () => {
     const [idx, setIdx] = useState("");
     const navigate = useNavigate();
 
+    const getPost = async() => {
+      axios.get(`http://localhost:4000/getPost/${props.idx}`)
+      .then(res=>(setPost(res.data),setIdx(res.data[0].idx),setTitle(res.data[0].title),setContent(res.data[0].content)))
+    }
+    
     useEffect(()=>{
-        axios.get(`http://localhost:4000/getPost/${location.state.idx}`)
-        .then(res=>(setPost(res.data),setIdx(res.data[0].idx),setTitle(res.data[0].title),setContent(res.data[0].content))) 
-    },[]);
+      getPost();
+    },[props.idx]);
 
     const updateText = () =>{
       inputDisable === true ? setInputDisable(false) : setInputDisable(true);
     };
+
+    const showPost = (showPostVal) => {
+      console.log("1:,",showPostVal)
+      props.showPost(showPostVal)
+    }
 
     const updateButtonClick = () => {
       setIdx(post[0].idx);
@@ -28,22 +37,22 @@ const Post = () => {
         title : title,
         content : content,
         idx:idx
-      }).then(alert("정상 수정되었습니다."))
+      }).then(alert("정상 수정되었습니다."), showPost("update"))
     };
 
     const deleteButtonClick = () => {
       setIdx(post[0].idx);
       axios.post(`http://localhost:4000/study/delete`, {
         idx: idx
-      }).then(alert("정상 삭제되었습니다."), navigate("/study"))
+      }).then(alert("정상 삭제되었습니다."), showPost("delete"))
     }; 
 
     
   return (
-    <div className='main'>
+    <div>
       <div>
-        <div>Post</div>
-        <button onClick={()=>{updateText()}}>수정</button><button onClick={()=>{updateButtonClick()}}>수정하기</button><button onClick={()=>{deleteButtonClick()}}>삭제하기</button>
+        <div>Post<button onClick={()=>{updateText()}}>수정</button><button onClick={()=>{updateButtonClick()}}>수정하기</button><button onClick={()=>{deleteButtonClick()}}>삭제하기</button></div>
+        
         <div>
           <table>
             <tr><td>{post&&post[0].idx}</td><td><input type="text" id="title" onChange={(e)=>setTitle(e.target.value)} defaultValue={post&&post[0].title} disabled={inputDisable}/></td><td><input type="text" id="title" onChange={(e)=>setContent(e.target.value)} defaultValue={post&&post[0].content} disabled={inputDisable}/></td></tr>
--- src/page/Study.js
+++ src/page/Study.js
@@ -2,50 +2,48 @@ import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
-import { spaceAction } from '../redux/actions/spaceAction'
+import Post from './Post'
 
 const Study = () => {
   const [studyList, setStudyList] = useState(null);
-  const navigate = useNavigate("");
-  const dispatch = useDispatch();
   const [viewPost, setViewPost] = useState("");
-  let stateValue = useSelector((state)=>state);
+  const [updateIdx, setUpadateIdx] = useState("");
+
+  const getStudyList = (postValue) => {
+    axios.get("http://localhost:4000/getStudyList")
+    .then(res=>setStudyList(res.data), postValue==="update"?setViewPost(updateIdx):setViewPost(""))
+  };
 
   useEffect(()=>{
-    axios.get("http://localhost:4000/getStudyList")
-    .then(res=>setStudyList(res.data))
+    getStudyList()
   },[])
 
   const postClick = (idx) => {
     console.log(idx)
-    // post 호출해서 컴포넌트로 사용
-    navigate('/study/post', {state:{idx:idx}})
+    setViewPost(idx);
+    setUpadateIdx(idx);
   };
 
+  const showPost = (showPost) => { 
+    if(showPost=="delete"){getStudyList("delete")} 
+    if(showPost=="update"){getStudyList("update")}
+  };
 
-  console.log("stateValue : ")
-
-  const dispatchClick = () => {
-      dispatch(spaceAction.addSpace({text:"text"}))
-  }
-
-  const selectorClick = () => {
-      console.log("state : ", stateValue);
-  }
   return (
     <div className='main'>
       {/* Left */}
       <div style={{width:"20%", padding:"5px", border:"1px solid red"}}>
         <Link to="/study/create" >글쓰기</Link>
-        <button onClick={()=>dispatchClick()}>useDispatch</button>
-        <button onClick={()=>selectorClick()}>useSelector</button>
         <table>
           <tbody>
-          {studyList&&studyList.map((item,index)=>(<tr onClick={()=>{postClick(item.idx)}} key={index}><td>{item.idx}</td><td>{item.title}</td></tr>))}
+          {studyList!==null&&studyList.map((item,index)=>(<tr onClick={()=>{postClick(item.idx)}} key={index}><td>{item.idx}</td><td>{item.title}</td></tr>))}
           </tbody>
         </table>
+      </div>
+      {/* Right */}
+      <div style={{width:"80%", padding:"5px", border:"1px solid blue"}}>
+        {viewPost !== "" ? <Post idx={viewPost} showPost={showPost}/>:''}
       </div>
     </div>
   )
